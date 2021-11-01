44f621bfdc287b6a9b9f982ce5da53b834614588
kpovel <kar.pavel123@gamil.com>
2021-11-01 21:43:50 +0200

    calc

---
+++ script.js
@@ -0,0 +1,25 @@
+function calc(a, b, operator) {
+    let result;
+    const isNotValid = (typeof a != 'number') || (typeof b != 'number');
+    if (isNotValid) {
+        result = 'Error!'
+    } else if (!operator) {
+        result = 'Unknown operator'
+    } else if (operator === 'sum') {
+        result = a + b
+    } else if (operator === 'min') {
+        result = a - b
+    } else if (operator === 'multi') {
+        result = a * b
+    } else if (operator === 'divide') {
+        if (b === 0) {
+            result = 'Division by zero is imposible'
+        } else {
+            result = a / b
+        }
+    }
+    return result;
+}
+
+let result = (calc(9, 3, 'min'))
+console.log(result);
